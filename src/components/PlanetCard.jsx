11df90c029d8704aac22b0c1ad7b042a3cab268a
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-06 18:16:36 -0300

    Task 5 done

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PlanetCard = class extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
+      </div>
+    );
+  }
+};
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
